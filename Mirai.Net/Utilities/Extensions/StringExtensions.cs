d491057922edac5616551cab102cea10c212333b
AHpx <ahpx@yandex.com>
2021-05-25 22:08:15 +0800

    Added StringExtensions.cs and insert ToUri method

---
+++ Mirai.Net/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mirai.Net.Utilities.Extensions
+{
+    public static class StringExtensions
+    {
+        public static Uri ToUri(this string s)
+        {
+            try
+            {
+                Uri.TryCreate(s, UriKind.RelativeOrAbsolute, out var re);
+                
+                return re;
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid uri format!");
+            }
+        }
+    }
+}
