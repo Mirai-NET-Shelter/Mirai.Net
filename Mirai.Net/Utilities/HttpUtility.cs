cf0e878c1a381fc0986f2694187d2d1edc57aa35
AHpx <ahpx@yandex.com>
2021-05-25 22:08:00 +0800

    Added HttpUtility.cs and insert Get method

---
+++ Mirai.Net/Utilities/HttpUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+
+namespace Mirai.Net.Utilities
+{
+    internal static class HttpUtility
+    {
+        private static readonly RestClient RestClient;
+        static HttpUtility()
+        {
+            RestClient = new RestClient();
+        }
+
+        internal static async Task<IRestResponse> Get(string url)
+        {
+            return await RestClient.ExecuteAsync(new RestRequest(), Method.GET);
+        }
+    }
+}
