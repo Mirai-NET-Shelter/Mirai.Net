1a7075941d33ac6dccddb10cf8505dee4b9c5ade
AHpx <ahpx@yandex.com>
2021-05-25 21:49:47 +0800

    Build up

---
+++ Mirai.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mirai.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
