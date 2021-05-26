03676125a987921d3322faa689f13862786623bb
AHpx <ahpx@yandex.com>
2021-05-26 21:59:52 +0800

    Added ToJsonTest

---
+++ Mirai.Net.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mirai.Net.Utilities.Extensions;
+using Xunit;
+
+namespace Mirai.Net.Test
+{
+    public class Program
+    {
+        [Theory]
+        [InlineData(typeof(MyClass))]
+        internal void ToJson(Type type)
+        {
+            type.ToJson();
+        }
+    }
+
+    class MyClass
+    {
+        public string Name { get; set; }
+        public string Identify { get; set; }
+    }
+}
