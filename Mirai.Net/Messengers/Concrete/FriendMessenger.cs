422aa2c5f971e7393cfe4a7d3a8be2d8dc690e8d
AHpx <agpx@yandex.com>
2021-05-27 16:21:16 +0800

    Added FriendMessenger.cs

---
+++ Mirai.Net/Messengers/Concrete/FriendMessenger.cs
@@ -0,0 +1,12 @@
+﻿using Mirai.Net.Data.Messages;
+
+namespace Mirai.Net.Messengers.Concrete
+{
+    public class FriendMessenger : MessengerBase
+    {
+        public override void Send(MessageBase message)
+        {
+            
+        }
+    }
+}
