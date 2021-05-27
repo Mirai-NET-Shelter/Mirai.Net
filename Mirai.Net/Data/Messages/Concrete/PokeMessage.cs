030b0812cf0fd9534dcc9c657168686af67a8a05
AHpx <ahpx@yandex.com>
2021-05-27 21:59:23 +0800

    Added PokeMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/PokeMessage.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class PokeMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Poke";
+        
+        [JsonProperty("name")]
+        public string Name {get; set;}
+
+        public PokeMessage(string name = null)
+        {
+            Name = name;
+        }
+    }
+}
