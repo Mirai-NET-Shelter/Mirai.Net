3204a7eea120c212e4092d56813a411929a2c1ba
AHpx <ahpx@yandex.com>
2021-05-27 21:40:09 +0800

    Added AtMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/AtMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class AtMessage : MessageBase
+    {
+        public override string Type { get; set; } = "At";
+        
+        [JsonProperty("target")]
+        public string Target {get; set;}
+        
+        [JsonProperty("display")]
+        public string Display {get; set;}
+
+        public AtMessage(string target = null, string display = null)
+        {
+            Target = target;
+            Display = display;
+        }
+    }
+}
