93490005618acba16cd2668ca62164f0709c3fea
AHpx <agpx@yandex.com>
2021-05-27 17:08:10 +0800

    Added TempMessenger.cs

---
+++ Mirai.Net/Messengers/Concrete/TempMessenger.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Mirai.Net.Data.Messages;
+
+namespace Mirai.Net.Messengers.Concrete
+{
+    public class TempMessenger : MessengerBase
+    {
+        public override Task<MessageCallback> Send(params MessageBase[] message)
+        {
+            
+        }
+    }
+}
