f8f8d5016e8c053d4c976ea0eb4b9bf8cdb757be
AHpx <agpx@yandex.com>
2021-05-27 16:09:04 +0800

    Added QuoteMessage.cs

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using Mirai.Net.Data.Messages;
+using Mirai.Net.Data.Messages.Concrete;
 using Mirai.Net.Sessions;
 using Mirai.Net.Utilities.Extensions;
 using Newtonsoft.Json.Linq;
@@ -12,21 +14,29 @@ namespace Mirai.Net.Test
     {
         public static async Task Main()
         {
-            Bot.Session = new MiraiSession
+            var b = new SourceMessage
             {
-                Host = "127.0.0.1",
-                Port = "2334",
-                Key = "232511772e8745e0bd697f1dfb72f748",
-                QQ = "2672886221"
-            };;
-            await Bot.Launch();
+                Id = "114514",
+                Time = "2021"
+            };
+
+            Console.WriteLine(b.ToJson());
             
-            Console.WriteLine("Connected!");
-            Console.WriteLine(await Bot.GetPluginVersion());
-            await Task.Delay(5000);
-            
-            await Bot.Terminate();
-            Console.WriteLine("Disconnected!");
+            // Bot.Session = new MiraiSession
+            // {
+            //     Host = "127.0.0.1",
+            //     Port = "2334",
+            //     Key = "232511772e8745e0bd697f1dfb72f748",
+            //     QQ = "2672886221"
+            // };;
+            // await Bot.Launch();
+            //
+            // Console.WriteLine("Connected!");
+            // Console.WriteLine(await Bot.GetPluginVersion());
+            // await Task.Delay(5000);
+            //
+            // await Bot.Terminate();
+            // Console.WriteLine("Disconnected!");
         }
     }
 }
---
+++ Mirai.Net/Data/Messages/Concrete/QuoteMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class QuoteMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Quote";
+
+        [JsonProperty("id")] 
+        public string Id { get; set; }
+
+        [JsonProperty("groupId")] 
+        public string GroupId { get; set; }
+
+        [JsonProperty("senderId")] 
+        public string SenderId { get; set; }
+
+        [JsonProperty("targetId")] 
+        public string TargetId { get; set; }
+
+        [JsonProperty("origin")] 
+        public IEnumerable<MessageBase> Origin { get; set; }
+    }
+}
