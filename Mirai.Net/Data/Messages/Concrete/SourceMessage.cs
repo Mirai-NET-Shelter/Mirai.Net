6ed97b2657ce48f150f859e785deaea9d729882d
AHpx <agpx@yandex.com>
2021-05-27 16:00:41 +0800

    Added SourceMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/SourceMessage.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class SourceMessage : MessageBase
+    {
+        [JsonProperty("type")]
+        public override string Type { get; set; } = "Source";
+        
+        [JsonProperty("id")]
+        public new string Id { get; set; }
+        
+        [JsonProperty("time")]
+        public new string Time { get; set; }
+    }
+}
