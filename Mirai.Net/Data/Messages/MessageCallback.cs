7163933b0fc4784cdac38b558309c24db1cc314c
AHpx <agpx@yandex.com>
2021-05-27 16:54:46 +0800

    Added MessageCallback.cs

---
+++ Mirai.Net/Data/Messages/MessageCallback.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages
+{
+    public class MessageCallback
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        
+        [JsonProperty("msg")]
+        public string Message {get; set;}
+        
+        [JsonProperty("messageId")]
+        public string MessageId {get; set;}
+    }
+}
