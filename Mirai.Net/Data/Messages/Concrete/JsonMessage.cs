3a06178f9379309db126e26125b0777decaf7774
AHpx <ahpx@yandex.com>
2021-05-27 21:57:22 +0800

    Added JsonMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/JsonMessage.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class JsonMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Json";
+        
+        [JsonProperty("json")]
+        public string Json {get; set;}
+
+        public JsonMessage(string json = null)
+        {
+            Json = json;
+        }
+    }
+}
