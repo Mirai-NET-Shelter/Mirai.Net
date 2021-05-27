63a1cf043b16b9d4cf07b155a717122112b6e720
AHpx <agpx@yandex.com>
2021-05-27 16:13:33 +0800

    Added PlainMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/PlainMessage.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class PlainMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Plain";
+        
+        [JsonProperty("text")]
+        public string Text {get; set;}
+    }
+}
