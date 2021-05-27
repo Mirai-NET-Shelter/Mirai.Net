cfd170edc5cf8239ec853002709a256d50a4f217
AHpx <ahpx@yandex.com>
2021-05-27 21:45:00 +0800

    Added FaceMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/FaceMessage.cs
@@ -0,0 +1,28 @@
+﻿using Mirai.Net.Utilities.Extensions;
+using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class FaceMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Face";
+        
+        [JsonProperty("faceId")]
+        public string FaceId {get; set;}
+        
+        [JsonProperty("name")]
+        public string Name {get; set;}
+
+        public FaceMessage(string faceIdOrName)
+        {
+            if (faceIdOrName.IsNumber())
+            {
+                FaceId = faceIdOrName;
+            }
+            else
+            {
+                Name = faceIdOrName;
+            }
+        }
+    }
+}
