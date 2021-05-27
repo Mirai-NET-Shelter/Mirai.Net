e8634940dc18e1f81a23899c19b8ab02bee97aaf
AHpx <ahpx@yandex.com>
2021-05-27 23:32:35 +0800

    Accomplished VoiceUploader.cs

---
+++ Mirai.Net/Data/Messengers/Media/VoiceUploadCallback.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messengers.Media
+{
+    public class VoiceUploadCallback
+    {
+        [JsonProperty("voiceId")]
+        public string VoiceId {get; set;}
+        
+        [JsonProperty("url")]
+        public string Url {get; set;}
+        
+        [JsonProperty("path")]
+        public string Path {get; set;}
+    }
+}
---
+++ Mirai.Net/Messengers/MediaUploader/VoiceUploader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Mirai.Net.Data.Messengers.Enums;
+using Mirai.Net.Data.Messengers.Media;
+using Mirai.Net.Utilities;
+using Mirai.Net.Utilities.Extensions;
+
+namespace Mirai.Net.Messengers.MediaUploader
+{
+    public class VoiceUploader
+    {
+        public static async Task<VoiceUploadCallback> Upload(byte[] file, ImageUploaderType type = ImageUploaderType.Group)
+        {
+            //目前仅支持group
+            var requestType = "group";
+            var result = await HttpUtility.Post($"{Bot.Session.GetUrl()}/uploadVoice",
+                file, new
+                {
+                    sessionKey = Bot.Session.SessionKey,
+                    type = requestType
+                }.ToJson());
+
+            return result.Content.ToObject<VoiceUploadCallback>();
+        }
+        
+        public static async Task<VoiceUploadCallback> Upload(string filePath, ImageUploaderType type = ImageUploaderType.Group)
+        {
+            return await Upload(await File.ReadAllBytesAsync(filePath), type);
+        }
+    }
+}
