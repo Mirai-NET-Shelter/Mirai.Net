0e8b565264396dba4b028cfe51dc2600595def03
AHpx <agpx@yandex.com>
2021-05-27 16:00:25 +0800

    Added protected modify

--- Mirai.Net/Data/Messages/MessageBase.cs
+++ Mirai.Net/Data/Messages/MessageBase.cs
@@ -4,44 +4,47 @@ namespace Mirai.Net.Data.Messages
 {
     public abstract class MessageBase
     {
+        /// <summary>
+        /// 类型
+        /// </summary>
         public abstract string Type { get; set; }
 
-        public virtual string Id { get; set; }
-        public virtual string Time { get; set; }
-        public virtual string GroupId { get; set; }
-        public virtual string SenderId { get; set; }
-        public virtual string TargetId { get; set; }
-        public virtual IEnumerable<MessageBase> Origin { get; set; }
-        public virtual string Target { get; set; }
-        public virtual string Display { get; set; }
-        public virtual string FaceId { get; set; }
-        public virtual string Name { get; set; }
-        public virtual string Test { get; set; }
-        public virtual string ImageId { get; set; }
-        public virtual string Url { get; set; }
-        public virtual string Path { get; set; }
-        public virtual string VoiceId { get; set; }
-        public virtual string Xml { get; set; }
-        public virtual string Json { get; set; }
-        public virtual string App { get; set; }
-        public virtual string Title { get; set; }
-        public virtual string Brief { get; set; }
-        public virtual string Source { get; set; }
-        public virtual string Summary { get; set; }
-        public virtual IEnumerable<Node> NodeList { get; set; }
-        public virtual string InternalId { get; set; }
-        public virtual string Size { get; set; }
-        public virtual string Kind { get; set; }
-        public virtual string JumpUrl { get; set; }
-        public virtual string PictureUrl { get; set; }
-        public virtual string MusicUrl { get; set; }
+        protected virtual string Id { get; set; }
+        protected virtual string Time { get; set; }
+        protected virtual string GroupId { get; set; }
+        protected virtual string SenderId { get; set; }
+        protected virtual string TargetId { get; set; }
+        protected virtual IEnumerable<MessageBase> Origin { get; set; }
+        protected virtual string Target { get; set; }
+        protected virtual string Display { get; set; }
+        protected virtual string FaceId { get; set; }
+        protected virtual string Name { get; set; }
+        protected virtual string Test { get; set; }
+        protected virtual string ImageId { get; set; }
+        protected virtual string Url { get; set; }
+        protected virtual string Path { get; set; }
+        protected virtual string VoiceId { get; set; }
+        protected virtual string Xml { get; set; }
+        protected virtual string Json { get; set; }
+        protected virtual string App { get; set; }
+        protected virtual string Title { get; set; }
+        protected virtual string Brief { get; set; }
+        protected virtual string Source { get; set; }
+        protected virtual string Summary { get; set; }
+        protected virtual IEnumerable<Node> NodeList { get; set; }
+        protected virtual string InternalId { get; set; }
+        protected virtual string Size { get; set; }
+        protected virtual string Kind { get; set; }
+        protected virtual string JumpUrl { get; set; }
+        protected virtual string PictureUrl { get; set; }
+        protected virtual string MusicUrl { get; set; }
         
         public class Node
         {
-            public string SenderId { get; set; }
-            public string Time { get; set; }
-            public string SenderName { get; set; }
-            public IEnumerable<MessageBase> MessageChain { get; set; }
+            protected string SenderId { get; set; }
+            protected string Time { get; set; }
+            protected string SenderName { get; set; }
+            protected IEnumerable<MessageBase> MessageChain { get; set; }
         }
     }
 }
