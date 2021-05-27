eb881a869c262d317da5cc2528ca81495a15b5ff
AHpx <ahpx@yandex.com>
2021-05-27 22:11:08 +0800

    Added FileMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/FileMessage.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class FileMessage : MessageBase
+    {
+        public override string Type { get; set; } = "File";
+        
+        [JsonProperty("id")]
+        public string Id {get; set;}
+        
+        [JsonProperty("internalId")]
+        public string InternalId {get; set;}
+        
+        [JsonProperty("name")]
+        public string Name {get; set;}
+        
+        [JsonProperty("size")]
+        public string Size {get; set;}
+
+        public FileMessage(string id = null, string internalId = null, string name = null, string size = null)
+        {
+            Id = id;
+            InternalId = internalId;
+            Name = name;
+            Size = size;
+        }
+    }
+}
