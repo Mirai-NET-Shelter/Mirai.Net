bc67322a28660aa4a1d62879656f95bc2a6ad80e
AHpx <ahpx@yandex.com>
2021-05-27 21:56:45 +0800

    Added XmlMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/XmlMessage.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class XmlMessage : MessageBase
+    {
+        public override string Type { get; set; } = "Xml";
+        
+        [JsonProperty("xml")]
+        public string Xml {get; set;}
+
+        public XmlMessage(string xml = null)
+        {
+            Xml = xml;
+        }
+    }
+}
