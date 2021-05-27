bba197c9775ee4ddf37f124e85a2ab6ec243a077
AHpx <ahpx@yandex.com>
2021-05-27 21:58:19 +0800

    Added AppMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/AppMessage.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class AppMessage : MessageBase
+    {
+        public override string Type { get; set; } = "App";
+        
+        [JsonProperty("content")]
+        public string Content {get; set;}
+
+        public AppMessage(string content = null)
+        {
+            Content = content;
+        }
+    }
+}
