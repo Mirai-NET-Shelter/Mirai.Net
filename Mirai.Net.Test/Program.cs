8f79fd0265dded39701876b9d3eee0bf2794b919
AHpx <agpx@yandex.com>
2021-05-27 15:40:15 +0800

    Added MessageBase.cs

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -5,22 +5,20 @@ using System.Threading.Tasks;
 using Mirai.Net.Sessions;
 using Mirai.Net.Utilities.Extensions;
 using Newtonsoft.Json.Linq;
-
+//TODO: Edit unused modify to internal
 namespace Mirai.Net.Test
 {
     public class Program
     {
         public static async Task Main()
         {
-            var session = new MiraiSession
+            Bot.Session = new MiraiSession
             {
                 Host = "127.0.0.1",
                 Port = "2334",
                 Key = "232511772e8745e0bd697f1dfb72f748",
                 QQ = "2672886221"
-            };
-
-            Bot.Session = session;
+            };;
             await Bot.Launch();
             
             Console.WriteLine("Connected!");
@@ -30,27 +28,5 @@ namespace Mirai.Net.Test
             await Bot.Terminate();
             Console.WriteLine("Disconnected!");
         }
-
-        public static async void T1()
-        {
-            await Task.Delay(1000);
-            var a = 100;
-
-            Console.WriteLine(a / 0);
-        }
-
-        public static async Task T2()
-        {
-            await Task.Delay(1000);
-            var a = 100;
-
-            Console.WriteLine(a / 0);
-        }
-    }
-
-    class MyClass
-    {
-        public string Name { get; set; }
-        public string Identify { get; set; }
     }
 }
---
+++ Mirai.Net/Data/Messages/MessageBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Mirai.Net.Data.Messages
+{
+    public abstract class MessageBase
+    {
+        public abstract string Type { get; set; }
+
+        public virtual string Id { get; set; }
+        public virtual string Time { get; set; }
+        public virtual string GroupId { get; set; }
+        public virtual string SenderId { get; set; }
+        public virtual string TargetId { get; set; }
+        public virtual IEnumerable<MessageBase> Origin { get; set; }
+        public virtual string Target { get; set; }
+        public virtual string Display { get; set; }
+        public virtual string FaceId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Test { get; set; }
+        public virtual string ImageId { get; set; }
+        public virtual string Url { get; set; }
+        public virtual string Path { get; set; }
+        public virtual string VoiceId { get; set; }
+        public virtual string Xml { get; set; }
+        public virtual string Json { get; set; }
+        public virtual string App { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Brief { get; set; }
+        public virtual string Source { get; set; }
+        public virtual string Summary { get; set; }
+        public virtual IEnumerable<Node> NodeList { get; set; }
+        public virtual string InternalId { get; set; }
+        public virtual string Size { get; set; }
+        public virtual string Kind { get; set; }
+        public virtual string JumpUrl { get; set; }
+        public virtual string PictureUrl { get; set; }
+        public virtual string MusicUrl { get; set; }
+        
+        public class Node
+        {
+            public string SenderId { get; set; }
+            public string Time { get; set; }
+            public string SenderName { get; set; }
+            public IEnumerable<MessageBase> MessageChain { get; set; }
+        }
+    }
+}
