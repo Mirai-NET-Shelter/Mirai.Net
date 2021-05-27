c4253507191b02e1fde7335b9ece1385ec15274b
AHpx <agpx@yandex.com>
2021-05-27 17:39:45 +0800

    Added GroupMessenger.cs

---
+++ Mirai.Net/Messengers/Concrete/GroupMessenger.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Mirai.Net.Data.Messages;
+
+namespace Mirai.Net.Messengers.Concrete
+{
+    public class GroupMessenger : MessengerBase
+    {
+        public override Task<MessageCallback> Send(params MessageBase[] message)
+        {
+            
+        }
+
+        public override Task<MessageCallback> Send(string messageId, params MessageBase[] message)
+        {
+            
+        }
+    }
+}
