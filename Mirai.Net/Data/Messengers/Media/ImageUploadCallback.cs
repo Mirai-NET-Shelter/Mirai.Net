32ccf45d5785522caf248acd5c5b29d3d4a867ff
AHpx <ahpx@yandex.com>
2021-05-27 22:33:24 +0800

    Added ImageUploadCallback.cs

---
+++ Mirai.Net/Data/Messengers/Media/ImageUploadCallback.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messengers.Media
+{
+    public class ImageUploadCallback
+    {
+        [JsonProperty("imageId")]
+        public string ImageId {get; set;}
+        
+        [JsonProperty("url")]
+        public string Url {get; set;}
+        
+        [JsonProperty("path")]
+        public string Path {get; set;}
+    }
+}
