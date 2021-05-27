e34438e34fe960013a22445706b082183620aac2
AHpx <agpx@yandex.com>
2021-05-27 16:19:52 +0800

    Added MessengerBase.cs

---
+++ Mirai.Net/Messengers/MessengerBase.cs
@@ -0,0 +1,9 @@
+﻿using Mirai.Net.Data.Messages;
+
+namespace Mirai.Net.Messengers
+{
+    public abstract class MessengerBase
+    {
+        public abstract void Send(MessageBase message);
+    }
+}
