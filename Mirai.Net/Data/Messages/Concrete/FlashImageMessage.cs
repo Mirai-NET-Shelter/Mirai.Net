db6350fe04bd740e9531f13e1a56614848e3e78a
AHpx <ahpx@yandex.com>
2021-05-27 21:51:58 +0800

    Added FlashImageMessage.cs

---
+++ Mirai.Net/Data/Messages/Concrete/FlashImageMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Mirai.Net.Data.Messages.Enums;
+using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concrete
+{
+    public class FlashImageMessage : MessageBase
+    {
+        public override string Type { get; set; } = "FlashImage";
+        
+        [JsonProperty("imageId")]
+        public string ImageId {get; set;}
+        
+        [JsonProperty("url")]
+        public string Url {get; set;}
+        
+        [JsonProperty("path")]
+        public string Path {get; set;}
+        
+        public FlashImageMessage(string param, ImageMessageType type = ImageMessageType.Url)
+        {
+            _ = type switch
+            {
+                ImageMessageType.Id => ImageId = param,
+                ImageMessageType.Url => Url = param,
+                ImageMessageType.Path => Path = param,
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+        }
+    }
+}
