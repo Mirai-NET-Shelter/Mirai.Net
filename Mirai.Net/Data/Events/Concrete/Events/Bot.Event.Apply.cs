1b005f858e04ca64e0a4a650cb917197584f1475
AHpx <ahpx@yandex.com>
2021-05-29 00:14:20 +0800

    Added NewFriendApplyEventArgs.cs

--- Mirai.Net/Data/Events/Concrete/Events/Bot.Event.Apply.cs
+++ Mirai.Net/Data/Events/Concrete/Events/Bot.Event.Apply.cs
@@ -1,14 +1,20 @@
-﻿using Mirai.Net.Utilities.Extensions;
+﻿using System;
+using Mirai.Net.Data.Events.Concrete.Args.Apply;
+using Mirai.Net.Utilities.Extensions;
 
 namespace Mirai.Net
 {
     public static partial class Bot
     {
+        public static event Action<NewFriendApplyEventArgs> ReceivedFriendRequest;
+        
         private static void MatchApplyEvents(string data)
         {
             switch (data.ToJObject().GetPropertyValue("type"))
             {
-                
+                case "NewFriendRequestEvent":
+                    ReceivedFriendRequest?.Invoke(data.ToObject<NewFriendApplyEventArgs>());
+                    break;
             }
         }
     }
