8a0b4a818581b1c9639abdb7969a3bc1c7fb2927
AHpx <ahpx@yandex.com>
2021-05-28 23:20:02 +0800

    Added GroupNameChangedEventArgs.cs

--- Mirai.Net/Data/Events/Concrete/Events/Bot.Event.Group.cs
+++ Mirai.Net/Data/Events/Concrete/Events/Bot.Event.Group.cs
@@ -1,16 +1,20 @@
-﻿using Mirai.Net.Utilities.Extensions;
+﻿using System;
+using Mirai.Net.Data.Events.Concrete.Args.Group;
+using Mirai.Net.Utilities.Extensions;
 
 namespace Mirai.Net
 {
     public static partial class Bot
     {
-        
-        
+        public static event Action<GroupNameChangedEventArgs> GroupNameChanged;
+
         private static void MatchGroupEvents(string data)
         {
             switch (data.ToJObject().GetPropertyValue("type"))
             {
-                
+                case "GroupNameChangeEvent":
+                    GroupNameChanged?.Invoke(data.ToObject<GroupNameChangedEventArgs>());
+                    break;
             }
         }
     }
