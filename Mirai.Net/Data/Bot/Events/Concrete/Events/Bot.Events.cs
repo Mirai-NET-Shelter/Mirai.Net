74f3d475c19e644755aff9b843bcef9149980d1b
AHpx <ahpx@yandex.com>
2021-05-28 22:20:12 +0800

    Added Bot.Events.cs

---
+++ Mirai.Net/Data/Bot/Events/Concrete/Events/Bot.Events.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Mirai.Net.Data.Bot.Events.Concrete.Args;
+
+// ReSharper disable once CheckNamespace
+namespace Mirai.Net
+{
+    public static partial class Bot
+    {
+        public static event Action<BotMutedEventArgs> BotMuted;
+    }
+}
