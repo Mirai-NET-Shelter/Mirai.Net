3d54c470f0f924e1fd99b0a6fe30146db38637f8
AHpx <agpx@yandex.com>
2021-05-28 18:55:47 +0800

    Added OperationSender.cs

---
+++ Mirai.Net/Data/OperationSender.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data
+{
+    public class OperationSender
+    {
+        [JsonProperty("id")]
+        public string Id {get; set;}
+        
+        [JsonProperty("name")]
+        public string Name {get; set;}
+        
+        [JsonProperty("permission")]
+        public string Permission {get; set;}
+    }
+}
