414b1c3f57a6d9f69a6eacf3b8cb983a4ba7379e
AHpx <ahpx@yandex.com>
2021-05-28 23:41:48 +0800

    Added GroupMember.cs

---
+++ Mirai.Net/Data/GroupMember.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data
+{
+    public class GroupMember
+    {
+        [JsonProperty("id")]
+        public string Id {get; set;}
+            
+        [JsonProperty("memberName")]
+        public string Name {get; set;}
+            
+        [JsonProperty("permission")]
+        public MemberPermissionType Permission {get; set;}
+            
+        [JsonProperty("group")]
+        public OperationSenderGroup Group {get; set;}
+    }
+}
