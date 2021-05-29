66aa8afcafa8739eb9a74306a03620bb01e493bf
AHpx <agpx@yandex.com>
2021-05-29 17:14:44 +0800

    Removed redundancy output statement

--- Mirai.Net/Bot.cs
+++ Mirai.Net/Bot.cs
@@ -112,8 +112,6 @@ namespace Mirai.Net
                 operate = accept ? 0 : 1,
                 message = responseMessage
             }.ToJson());
-
-            Console.WriteLine(re.Content);
         }
 
         public static async Task<IEnumerable<BotFriend>> GetFriendList()
