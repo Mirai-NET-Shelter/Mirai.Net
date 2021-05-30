cd08ea28e4d638a006c16160aa5736b6c46684cd
AHpx <ahpx@yandex.com>
2021-05-30 22:05:26 +0800

    Added untested Move method

--- Mirai.Net/Managers/GroupFileManager.cs
+++ Mirai.Net/Managers/GroupFileManager.cs
@@ -72,5 +72,23 @@ namespace Mirai.Net.Managers
                 throw new Exception(jObj.GetPropertyValue("msg"));
             }
         }
+
+        public async Task Move(string fileId, string newPath)
+        {
+            var result = await HttpUtility.Post($"{Bot.Session.GetUrl()}/groupFileMove", new
+            {
+                sessionKey = Bot.Session.SessionKey,
+                target = GroupId,
+                id = fileId,
+                movePath = newPath
+            }.ToJson());
+
+            var jObj = result.Content.ToJObject();
+
+            if (jObj.GetPropertyValue("code") != "0")
+            {
+                throw new Exception(jObj.GetPropertyValue("msg"));
+            }
+        }
     }
 }
