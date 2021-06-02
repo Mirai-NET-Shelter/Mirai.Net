db021eab69c95357f9a18534b91b58542d72bb18
AHpx <agpx@yandex.com>
2021-06-02 13:57:35 +0800

    Updated deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:AHpxChina/Mirai.Net.git master:gh-pages
+git push -f https://github.com/AHpxChina/Mirai.Net.git master:gh-pages
 
 cd -
