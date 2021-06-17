4c7a5c7111c5631aa52799527a90c199383e0df6
AHpx <agpx@yandex.com>
2021-06-17 16:38:42 +0800

    Pending

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Mirai.Net.Sessions;
 using Newtonsoft.Json.Linq;
 
 namespace Mirai.Net.Test
 {
+    /*
+     * 这是一个基于.Net5的控制台应用程序，用于测试Mirai.Net主项目的各项功能是否能够正常运转
+     */
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            var bot = new MiraiBot
+            {
+                Address = "127.0.0.1:8080",
+                QQ = "2672886221",
+                VerifyKey = "1145141919810"
+            };
+
+            await bot.Launch();
         }
     }
 }
---
+++ Mirai.Net/Sessions/MiraiBot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace Mirai.Net.Sessions
+{
+    /// <summary>
+    /// 这是用来描述一个Mirai机器人的类
+    /// </summary>
+    public class MiraiBot
+    {
+        /// <summary>
+        /// Mirai.Net总是需要一个VerifyKey
+        /// </summary>
+        public string VerifyKey { get; set; }
+
+        /// <summary>
+        /// 新建连接 或 singleMode 模式下为空, 通过已有 sessionKey 连接时不可为空
+        /// </summary>
+        public string SessionKey { get; set; }
+
+        /// <summary>
+        /// 比如：localhost:114514
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 绑定的账号, singleMode 模式下为空, 非 singleMode 下新建连接不可为空
+        /// </summary>
+        public string QQ { get; set; }
+
+        public async Task Launch()
+        {
+            var url = new Uri($@"ws://{Address}");
+            var factor = new Func<ClientWebSocket>(() =>
+            {
+                var ws = new ClientWebSocket();
+
+                ws.Options.SetRequestHeader("verifyKey", VerifyKey);
+                ws.Options.SetRequestHeader("qq", QQ);
+                // ws.Options.SetRequestHeader("sessionKey", null);
+
+                return ws;
+            });
+
+            using var client = new WebsocketClient(url, factor);
+
+            client.MessageReceived.Subscribe(Console.WriteLine);
+            
+            await client.StartOrFail();
+        }
+    }
+}
