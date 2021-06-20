281eb77955ee28e31171588079460236f53561f5
AHpx <agpx@yandex.com>
2021-06-20 18:04:11 +0800

    Add FriendEvent.cs

---
+++ Mirai.Net/Data/Person/Friend.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Person
+{
+    /// <summary>
+    /// 好友对象
+    /// </summary>
+    public class Friend
+    {
+        /// <summary>
+        /// 好友的QQ号
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id {get; set;}
+        
+        /// <summary>
+        /// 好友的昵称
+        /// </summary>
+        [JsonProperty("nickname")]
+        public string NickName {get; set;}
+        
+        /// <summary>
+        /// 你给好友的备注
+        /// </summary>
+        [JsonProperty("remark")]
+        public string Remark {get; set;}
+    }
+}
