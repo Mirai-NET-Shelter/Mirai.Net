f380410e24511db7984572210643b66f59a80ad2
AHpx <agpx@yandex.com>
2021-06-21 18:08:55 +0800

    Add ApplyEventListener.cs

---
+++ Mirai.Net/Listeners/EventListeners/ApplyEventListener.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirai.Net.Data.Events;
+using Mirai.Net.Data.Events.Apply;
+using Mirai.Net.Data.Events.Group;
+
+namespace Mirai.Net.Listeners.EventListeners
+{
+    public class ApplyEventListener : IEventListener
+    {
+        public IEnumerable<EventType> EventTypes { get; set; } = new[]
+        {
+            EventType.NewFriendRequestEvent,
+            EventType.MemberJoinRequestEvent,
+            EventType.BotInvitedJoinGroupRequestEvent
+        };
+
+        public event Action<NewFriendRequestEventArgs> NewFriendRequest;
+        public event Action<MemberJoinRequestEventArgs> MemberJoinRequest;
+        public event Action<BotInvitedJoinGroupRequestEvent> BotInvited; 
+        
+        public void Execute(EventArgsBase args)
+        {
+            switch (args.Type)
+            {
+                case EventType.NewFriendRequestEvent:
+                    NewFriendRequest?.Invoke(args as NewFriendRequestEventArgs);
+                    break;
+                case EventType.MemberJoinRequestEvent:
+                    MemberJoinRequest?.Invoke(args as MemberJoinRequestEventArgs);
+                    break;
+                case EventType.BotInvitedJoinGroupRequestEvent:
+                    BotInvited?.Invoke(args as BotInvitedJoinGroupRequestEvent);
+                    break;
+            }
+        }
+    }
+}
