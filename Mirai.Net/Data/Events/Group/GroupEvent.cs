0262d6a2c4999b16f495e212b2404a9fd74cae0a
AHpx <agpx@yandex.com>
2021-06-21 15:54:54 +0800

    Add GroupMessageRecallEventArgs

---
+++ Mirai.Net/Data/Events/Group/GroupEvent.cs
@@ -0,0 +1,38 @@
+﻿using Mirai.Net.Data.Contact;
+using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Events.Group
+{
+    public class GroupRecallEventArgs : EventArgsBase
+    {
+        /// <summary>
+        /// 原消息发送者的QQ号
+        /// </summary>
+        [JsonProperty("authorId")]
+        public string AuthorId {get; set;}
+        
+        /// <summary>
+        /// 消息id
+        /// </summary>
+        [JsonProperty("messageId")]
+        public string MessageId {get; set;}
+        
+        /// <summary>
+        /// 撤回时间戳
+        /// </summary>
+        [JsonProperty("time")]
+        public string Time {get; set;}
+        
+        /// <summary>
+        /// 产生此事件的群
+        /// </summary>
+        [JsonProperty("group")]
+        public Contact.Group Group {get; set;}
+        
+        /// <summary>
+        /// 操作者
+        /// </summary>
+        [JsonProperty("operator")]
+        public GroupActionOperator Operator {get; set;}
+    }
+}
