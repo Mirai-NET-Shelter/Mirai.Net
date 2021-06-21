aacfd0494c2ff0602e9a6730365728eb230330bc
AHpx <agpx@yandex.com>
2021-06-21 15:01:39 +0800

    Accomplish BotGroupPermissionChangeEventArgs

--- Mirai.Net/Data/Events/Bot/GroupBotEvent.cs
+++ Mirai.Net/Data/Events/Bot/GroupBotEvent.cs
@@ -1,7 +1,20 @@
-﻿namespace Mirai.Net.Data.Events.Bot
+﻿using Mirai.Net.Data.Contact;
+using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Events.Bot
 {
+    /// <summary>
+    /// Bot在群里的权限被改变. 操作人一定是群主
+    /// </summary>
     public class BotGroupPermissionChangeEventArgs : EventArgsBase
     {
+        [JsonProperty("origin")]
+        public GroupPermission Origin {get; set;}
         
+        [JsonProperty("current")]
+        public GroupPermission Current {get; set;}  
+        
+        [JsonProperty("group")]
+        public Group Group {get; set;}
     }
 }
