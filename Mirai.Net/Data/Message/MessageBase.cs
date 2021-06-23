1f821b7bcfbc105dd246d5c086913a64d63570ef
AHpx <agpx@yandex.com>
2021-06-23 17:15:49 +0800

    Add MessageBase.cs

---
+++ Mirai.Net/Data/Message/MessageBase.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Mirai.Net.Data.Message
+{
+    public class MessageBase
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty("type")]
+        public MessageType Type {get; protected set; }
+    }
+}
--- Mirai.Net/Data/Message/MessageType.cs
+++ Mirai.Net/Data/Message/MessageType.cs
@@ -1,26 +1,33 @@
-﻿namespace Mirai.Net.Data.Message
+﻿using System.Runtime.Serialization;
+
+namespace Mirai.Net.Data.Message
 {
     public enum MessageType
     {
         /// <summary>
         /// 群消息
         /// </summary>
+        [EnumMember (Value = "FriendMessage")]
         Group,
         /// <summary>
         /// 好友消息
         /// </summary>
+        [EnumMember (Value = "GroupMessage")]
         Friend,
         /// <summary>
         /// 群临时消息
         /// </summary>
+        [EnumMember (Value = "TempMessage")]
         Temp,
         /// <summary>
         /// 陌生人消息
         /// </summary>
+        [EnumMember (Value = "StrangerMessage")]
         Stranger,
         /// <summary>
         /// 其他客户端消息
         /// </summary>
+        [EnumMember (Value = "OtherClientMessage")]
         OtherClient
     }
 }
