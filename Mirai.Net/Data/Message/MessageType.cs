9f37aca48bd24957b5e57c51532977b01cbe0028
AHpx <agpx@yandex.com>
2021-06-23 16:21:16 +0800

    Add MessageType.cs

---
+++ Mirai.Net/Data/Message/MessageType.cs
@@ -0,0 +1,26 @@
+﻿namespace Mirai.Net.Data.Message
+{
+    public enum MessageType
+    {
+        /// <summary>
+        /// 群消息
+        /// </summary>
+        Group,
+        /// <summary>
+        /// 好友消息
+        /// </summary>
+        Friend,
+        /// <summary>
+        /// 群临时消息
+        /// </summary>
+        Temp,
+        /// <summary>
+        /// 陌生人消息
+        /// </summary>
+        Stranger,
+        /// <summary>
+        /// 其他客户端消息
+        /// </summary>
+        OtherClient
+    }
+}
