9246e1fea5829fa5607107a42c352b1389a5f8ae
AHpx <agpx@yandex.com>
2021-06-23 15:19:49 +0800

    Removed redundancy implements for IEventListener Migrate project framework to .net 5 instead of .net standard

--- Mirai.Net.Test/TestListener.cs
+++ Mirai.Net.Test/TestListener.cs
@@ -3,25 +3,21 @@ using Mirai.Net.Data.Events;
 using Mirai.Net.Data.Events.Friend;
 using Mirai.Net.Data.Events.Group;
 using Mirai.Net.Listeners;
-using Mirai.Net.Listeners.EventListeners;
 using Mirai.Net.Utils.Extensions;
 
 namespace Mirai.Net.Test
 {
-    public class TestListener : GroupEventListener
+    public class TestListener : IEventListener
     {
         public TestListener()
         {
-            MuteAll += args =>
-            {
-                Console.WriteLine(
-                    $"{args.Operator.Name} {(args.Current ? "muted" : "unmuted")} {args.Operator.Group.Name}!");
-            };
             
-            AllowAnonymousChatChange += e =>
-            {
-                Console.WriteLine($"{e.Operator.Name} changed the group {e.Group.Id} anonymous chat status from {e.Origin} to {e.Current}");
-            };
+        }
+
+        public IEnumerable<EventType> EventTypes { get; set; }
+        public void Execute(EventArgsBase args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
