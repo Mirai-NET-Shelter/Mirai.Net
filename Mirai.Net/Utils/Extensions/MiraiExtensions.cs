ffe4b2ee88352c2659ce0d32e533f6b4e83c517e
AHpx <agpx@yandex.com>
2021-07-04 18:13:32 +0800

    Add MiraiExtensions.cs

---
+++ Mirai.Net/Utils/Extensions/MiraiExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mirai.Net.Utils.Extensions
+{
+    public static class MiraiExtensions
+    {
+        /// <summary>
+        /// 根据mirai-http-api的响应json来判断对应请求是否成功。
+        /// </summary>
+        /// <param name="s">mirai的api响应json</param>
+        public static void EnsureSuccess(this string s)
+        {
+            var obj = s.ToJObject();
+            var code = obj.Fetch("code");
+
+            if (code != "0")
+            {
+                throw new Exception(obj.Fetch("msg"));
+            }
+        }
+    }
+}
