48da1e4608a8241eee3c6efb1e239cfcb45ce00d
AHpx <AHpx@yandex.com>
2021-07-30 01:20:39 +0800

    Testing dynamic json to entity object via reflection

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using AHpx.Extensions.StringExtensions;
+using Mirai.Net.Data.Events;
 using Mirai.Net.Data.Sessions;
 using Mirai.Net.Sessions;
 using Mirai.Net.Utils.Extensions;
+using Newtonsoft.Json;
 using Websocket.Client;
 
 namespace Mirai.Net.Test
@@ -27,18 +31,49 @@ namespace Mirai.Net.Test
             //
             // exit.WaitOne(TimeSpan.FromSeconds(30));
 
-            MyClass c = new MyClass2
-            {
-                Type = "MyClass2",
-                Name = "jack"
-            };
+            var json1 = new {Type = Events.GroupMessageRecalled}.ToJsonString();
+            var json2 = new {Type = Events.Online, Name = "AHpx"}.ToJsonString();
+            var json3 = new {Type = Events.Offline, Id = 114}.ToJsonString();
+            var json4 = new {Type = Events.Dropped, IsShit = true}.ToJsonString();
 
-            Console.WriteLine(c);
+            var jsons = new[] {json1, json2, json3, json4};
+            var types = new[] {typeof(MyClass2), typeof(MyClass3), typeof(MyClass4)};
+
+            var re = new List<MyClass>();
+            foreach (var json in jsons)
+            {
+                var root = JsonConvert.DeserializeObject<MyClass>(json);
+                
+                foreach (var type in types)
+                {
+                    var obj = JsonConvert.DeserializeObject(json, type);
+
+                    Console.WriteLine(obj.ToJsonString());
+
+                    var value = obj?.GetType().GetProperty("Type")?.GetValue(obj, null);
+                    if (value is Events events)
+                    {
+                        if (root.Type == events)
+                        {
+                            re.Add(obj as MyClass);
+                        }
+                    }
+                }
+            }
+            
+            // foreach (var myClass in re)
+            // {
+            //     Console.WriteLine(myClass.ToJsonString());
+            // }
         }
 
-        public abstract class  MyClass
+        public class MyClass
         {
-            public abstract string Type { get; set; }
+            protected MyClass()
+            {
+            }
+
+            public virtual Events Type { get; set; }
 
             public override string ToString()
             {
@@ -49,19 +84,19 @@ namespace Mirai.Net.Test
         public class MyClass2 : MyClass
         {
             public string Name { get; set; }
-            public override string Type { get; set; } = "MyClass2";
+            public override Events Type { get; set; } = Events.Online;
         }
 
         public class MyClass3 : MyClass
         {
-            public override string Type { get; set; } = "MyClass3";
+            public override Events Type { get; set; } = Events.Offline;
 
-            public int Id { get; set; }
+            public int Id { get; set; } = 10;
         }
         
         public class MyClass4 : MyClass
         {
-            public override string Type { get; set; } = "MyClass4";
+            public override Events Type { get; set; } = Events.Dropped;
 
             public bool IsShit { get; set; }
         }
