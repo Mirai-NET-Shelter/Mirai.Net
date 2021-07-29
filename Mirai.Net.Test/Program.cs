09fec18fc655542054d19b621989eb64b36f1bf4
AHpx <AHpx@yandex.com>
2021-07-29 23:54:07 +0800

    Add event enumerates

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
+using AHpx.Extensions.StringExtensions;
 using Mirai.Net.Data.Sessions;
 using Mirai.Net.Sessions;
 using Mirai.Net.Utils.Extensions;
@@ -13,26 +14,56 @@ namespace Mirai.Net.Test
     {
         private static async Task Main()
         {
-            var exit = new ManualResetEvent(false);
-            using var bot = new MiraiBot
-            {
-                Address = "localhost:8080",
-                QQ = 2672886221,
-                VerifyKey = "1145141919810"
-            };
-            
-            await bot.Launch();
-            Console.WriteLine(await bot.GetPluginVersion());
+            // var exit = new ManualResetEvent(false);
+            // using var bot = new MiraiBot
+            // {
+            //     Address = "localhost:8080",
+            //     QQ = 2672886221,
+            //     VerifyKey = "1145141919810"
+            // };
+            //
+            // await bot.Launch();
+            // Console.WriteLine(await bot.GetPluginVersion());
+            //
+            // exit.WaitOne(TimeSpan.FromSeconds(30));
 
-            exit.WaitOne(TimeSpan.FromSeconds(30));
+            MyClass c = new MyClass2
+            {
+                Type = "MyClass2",
+                Name = "jack"
+            };
+
+            Console.WriteLine(c);
         }
 
-        enum MyEnum
+        public abstract class  MyClass
         {
-            [Description("t1")]
-            Test1,
-            Test2,
-            Test3
+            public abstract string Type { get; set; }
+
+            public override string ToString()
+            {
+                return this.ToJsonString();
+            }
+        }
+        
+        public class MyClass2 : MyClass
+        {
+            public string Name { get; set; }
+            public override string Type { get; set; } = "MyClass2";
+        }
+
+        public class MyClass3 : MyClass
+        {
+            public override string Type { get; set; } = "MyClass3";
+
+            public int Id { get; set; }
+        }
+        
+        public class MyClass4 : MyClass
+        {
+            public override string Type { get; set; } = "MyClass4";
+
+            public bool IsShit { get; set; }
         }
     }
 }
---
+++ Mirai.Net/Data/Events/EventTargets.cs
@@ -0,0 +1,10 @@
+﻿namespace Mirai.Net.Data.Events
+{
+    public enum EventTargets
+    {
+        Bot,
+        Friend,
+        Group,
+        Request
+    }
+}
