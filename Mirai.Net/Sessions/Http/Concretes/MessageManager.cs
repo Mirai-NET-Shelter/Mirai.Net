a2063efc8ba9a206f19c54ea2a723dadd0a70da9
AHpx <AHpx@yandex.com>
2021-08-01 00:35:58 +0800

    Add MessageManager.cs

---
+++ Mirai.Net/Sessions/Http/Concretes/MessageManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirai.Net.Data.Messages;
+
+namespace Mirai.Net.Sessions.Http.Concretes
+{
+    public class MessageManager
+    {
+        public readonly MiraiBot Bot;
+
+        public MessageManager(MiraiBot bot)
+        {
+            Bot = bot;
+        }
+
+        public string SendFriendMessage(string target)
+        {
+            return "";
+        }
+    }
+}
