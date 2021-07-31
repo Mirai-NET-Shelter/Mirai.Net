4eb90aff43b6be5b0efebf65daaf780d0e138df0
AHpx <AHpx@yandex.com>
2021-07-31 21:58:53 +0800

    Add dependency-injection based ManagerBuilder.cs

---
+++ Mirai.Net/Sessions/Http/ManagerBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+
+namespace Mirai.Net.Sessions.Http
+{
+    public class ManagerBuilder
+    {
+        private ContainerBuilder _builder;
+        private IContainer _container => _builder.Build();
+
+        public ManagerBuilder(MiraiBot bot)
+        {
+            _builder = new ContainerBuilder();
+            _builder.Register(context => bot);
+        }
+
+        public T Build<T>()
+        {
+            _builder.RegisterType<T>();
+
+            return _container.Resolve<T>();
+        }
+    }
+}
