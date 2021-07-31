79b4f2352d66f469fca5b66bb96e56adbdd21d14
AHpx <AHpx@yandex.com>
2021-07-31 22:08:39 +0800

    Accomplish endpoints

--- Mirai.Net/Data/Sessions/HttpEndpoints.cs
+++ Mirai.Net/Data/Sessions/HttpEndpoints.cs
@@ -13,5 +13,43 @@ namespace Mirai.Net.Data.Sessions
         [Description("about")] About,
         
         [Description("friendList")] FriendList,
+        [Description("groupList")] GroupList,
+        [Description("memberList")] MemberList,
+        [Description("botProfile")] BotProfile,
+        [Description("friendProfile")] FriendProfile,
+        [Description("memberProfile")] MemberProfile,
+        
+        [Description("sendFriendMessage")] SendFriendMessage,
+        [Description("sendGroupMessage")] SendGroupMessage,
+        [Description("sendTempMessage")] SendTempMessage,
+        [Description("sendNudge")] SendNudge,
+        [Description("recall")] Recall,
+        
+        [Description("file/list")] FileList,
+        [Description("file/info")] FileInfo,
+        [Description("file/mkdir")] FileCreate,
+        [Description("file/delete")] FileDelete,
+        [Description("file/move")] FileMove,
+        [Description("file/rename")] FileRename,
+        [Description("file/upload")] FileUpload,
+        
+        [Description("uploadImage")] UploadImage,
+        [Description("uploadVoice")] UploadVoice,
+        
+        [Description("deleteFriend")] DeleteFriend,
+        
+        [Description("mute")] Mute,
+        [Description("unmute")] Unmute,
+        [Description("kick")] Kick,
+        [Description("quit")] Quit,
+        [Description("muteAll")] MuteAll,
+        [Description("unmuteAll")] UnmuteAll,
+        [Description("setEssence")] SetEssence,
+        [Description("groupConfig")] GroupConfig,
+        [Description("memberInfo")] MemberInfo,
+        
+        [Description("resp/newFriendRequestEvent")] NewFriendRequested,
+        [Description("resp/memberJoinRequestEvent")] MemberJoinRequested,
+        [Description("resp/botInvitedJoinGroupRequestEvent")] BotInvited
     }
 }
