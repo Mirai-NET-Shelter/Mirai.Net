bd7e782ed41285f287040b77c898dbf2d5775fdd
AHpx <AHpx@yandex.com>
2021-08-01 23:59:30 +0800

    Add group-related methods to GroupManagerExtensions.cs

--- Mirai.Net/Utils/Extensions/Managers/GroupManagerExtensions.cs
+++ Mirai.Net/Utils/Extensions/Managers/GroupManagerExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AHpx.Extensions.StringExtensions;
 using Mirai.Net.Data.Sessions;
+using Mirai.Net.Data.Shared;
 using Mirai.Net.Sessions.Http.Concretes;
 
 namespace Mirai.Net.Utils.Extensions.Managers
@@ -12,6 +14,31 @@ namespace Mirai.Net.Utils.Extensions.Managers
             var response = await manager.Bot.PostHttp(endpoints, payload, true);
 
             manager.Bot.EnsureSuccess(response);
+        }
+
+        public static async Task<IEnumerable<Member>> GetMembers(this GroupManager manager, string group)
+        {
+            return await manager.Bot.GetManager<AccountManager>().GetGroupMembers(group);
+        }
+        
+        public static async Task<IEnumerable<Member>> GetMembers(this GroupManager manager, long group)
+        {
+            return await manager.Bot.GetManager<AccountManager>().GetGroupMembers(group);
+        }
+
+        public static async Task<IEnumerable<Group>> GetGroups(this GroupManager manager)
+        {
+            return await manager.Bot.GetManager<AccountManager>().GetGroups();
+        }
+
+        public static async Task<Profile> GetMemberProfile(this GroupManager manager, string id, string target)
+        {
+            return await manager.Bot.GetManager<AccountManager>().GetMemberProfile(id, target);
+        }
+        
+        public static async Task<Profile> GetMemberProfile(this GroupManager manager, long id, long target)
+        {
+            return await manager.Bot.GetManager<AccountManager>().GetMemberProfile(id, target);
         }
     }
 }
