5b45af989796b9c6a0901231c1ea5e7f88a87fce
AHpx <AHpx@yandex.com>
2021-11-27 22:22:12 +0800

    Specified blank string for AtMessage.cs

--- Mirai.Net/Data/Messages/Concretes/AtMessage.cs
+++ Mirai.Net/Data/Messages/Concretes/AtMessage.cs
@@ -20,10 +20,10 @@ public class AtMessage : MessageBase
     public string Target { get; set; }
 
     /// <summary>
-    ///     At时显示的文字，发送消息时无效，自动使用群名片
+    ///     At时显示的文字，发送消息时无效，自动使用群名片，默认为空
     /// </summary>
     [JsonProperty("display")]
-    public string Display { get; set; }
+    public string Display { get; set; } = "";
 
     public override Messages Type { get; set; } = Messages.At;
 }
