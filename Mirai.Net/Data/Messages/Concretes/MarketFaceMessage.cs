ca7d557f21ae74640f551c5b524de879318d69a2
AHpx <AHpx@yandex.com>
2022-02-25 22:05:29 +0800

    Adapted MarketFaceMessage.cs

---
+++ Mirai.Net/Data/Messages/Concretes/MarketFaceMessage.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concretes;
+
+/// <summary>
+/// 商城表情
+/// <remarks>目前商城表情仅支持接收和转发，不支持构造发送</remarks>
+/// </summary>
+public class MarketFaceMessage : MessageBase
+{
+    public override Messages Type { get; set; } = Messages.MarketFace;
+    
+    [JsonProperty("id")]
+    public string Id {get; set;}
+    
+    [JsonProperty("name")]
+    public string Name {get; set;}
+}
