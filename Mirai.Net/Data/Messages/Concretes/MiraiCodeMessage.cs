deefdf18284c4af6a16f6ddd749f47eaa7633f5f
AHpx <AHpx@yandex.com>
2022-02-26 00:07:30 +0800

    Added MiraiCodeMessage.cs definition

---
+++ Mirai.Net/Data/Messages/Concretes/MiraiCodeMessage.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Mirai.Net.Data.Messages.Concretes;
+
+public class MiraiCodeMessage : MessageBase
+{
+    public override Messages Type { get; set; }
+    
+    [JsonProperty("code")]
+    public string Code {get; set;}
+}
--- Mirai.Net/Data/Messages/Messages.cs
+++ Mirai.Net/Data/Messages/Messages.cs
@@ -19,5 +19,6 @@ public enum Messages
     MusicShare,
     Forward,
     File,
-    MarketFace
+    MarketFace,
+    MiraiCode
 }
