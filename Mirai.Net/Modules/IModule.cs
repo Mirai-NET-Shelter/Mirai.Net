af480950d0c769a15ff0d5a4ffba3ad2cc0228c0
AHpx <agpx@yandex.com>
2022-03-02 16:26:55 +0800

    Added summary

--- Mirai.Net/Modules/IModule.cs
+++ Mirai.Net/Modules/IModule.cs
@@ -2,8 +2,19 @@
 
 namespace Mirai.Net.Modules;
 
+/// <summary>
+/// 模块化接口
+/// </summary>
 public interface IModule
 {
+    /// <summary>
+    /// 执行器
+    /// </summary>
+    /// <param name="base"></param>
     void Execute(MessageReceiverBase @base);
+    
+    /// <summary>
+    /// 是否启用模块
+    /// </summary>
     bool? IsEnable { get; set; }
 }
