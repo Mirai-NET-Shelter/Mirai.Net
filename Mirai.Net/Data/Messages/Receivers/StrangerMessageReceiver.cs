1c340b19d8cc98933faa32a3ddfb35cbff4a167f
AHpx <agpx@yandex.com>
2022-03-02 16:02:02 +0800

    Added summary and renamed non0\-standard named properties

--- Mirai.Net/Data/Messages/Receivers/StrangerMessageReceiver.cs
+++ Mirai.Net/Data/Messages/Receivers/StrangerMessageReceiver.cs
@@ -3,27 +3,30 @@ using Newtonsoft.Json;
 
 namespace Mirai.Net.Data.Messages.Receivers;
 
+/// <summary>
+/// 陌生人接收器
+/// </summary>
 public class StrangerMessageReceiver : MessageReceiverBase
 {
+    /// <summary>
+    /// 消息接收器类型
+    /// </summary>
     public override MessageReceivers Type { get; set; } = MessageReceivers.Stranger;
 
+    /// <summary>
+    /// 发送者
+    /// </summary>
     [JsonProperty("sender")] public Friend Sender { get; set; }
     
     /// <summary>
     /// 昵称
     /// </summary>
     [JsonIgnore]
-    public string Name => Sender.NickName;
-
-    /// <summary>
-    /// 备注
-    /// </summary>
-    [JsonIgnore]
-    public string Remark => Sender.Remark;
+    public string StrangerName => Sender.NickName;
 
     /// <summary>
     /// QQ号
     /// </summary>
     [JsonIgnore]
-    public string Id => Sender.Id;
+    public string StrangerId => Sender.Id;
 }
