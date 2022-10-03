04a66568121f1e2bf55ab9afde38e71269831f8f
AHpx <ahpx@yandex.com>
2022-10-03 14:37:29 +0800

    Removed redundant properties

--- Mirai.Net.Test/Program.cs
+++ Mirai.Net.Test/Program.cs
@@ -13,6 +13,7 @@ using Mirai.Net.Sessions;
 using Mirai.Net.Sessions.Http.Managers;
 using Mirai.Net.Utils.Internal;
 using Mirai.Net.Utils.Scaffolds;
+using Newtonsoft.Json;
 
 namespace Mirai.Net.Test
 {
@@ -40,7 +41,12 @@ namespace Mirai.Net.Test
                 {
                     if (r.MessageChain.GetPlainMessage() == "/t")
                     {
-                        var id = await r.SendMessageAsync("Echo");
+                        var id = await r.SendMessageAsync(new PlainMessage("Echo") + new ImageMessage
+                        {
+                            Url = "https://picsum.photos/200/300",
+                            Width = "200",
+                            Height = "300"
+                        });
                         var msg = await MessageManager
                             .GetMessageReceiverByIdAsync<GroupMessageReceiver>(id, r.GroupId);
 
--- Mirai.Net/Data/Messages/Concretes/ImageMessage.cs
+++ Mirai.Net/Data/Messages/Concretes/ImageMessage.cs
@@ -47,16 +47,4 @@ public record ImageMessage : MessageBase
     /// </summary>
     [JsonProperty("height")]
     public string Height { get; set; }
-
-    /// <summary>
-    ///     图片的类型
-    /// </summary>
-    [JsonProperty("imageType")]
-    public string ImageType { get; set; }
-
-    /// <summary>
-    ///     指示图片是否为Emoji
-    /// </summary>
-    [JsonProperty("isEmoji")]
-    public bool IsEmoji { get; set; }
 }
